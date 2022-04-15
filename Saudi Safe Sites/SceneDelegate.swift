b98d53cfe8f28d94900bc166d9a68ab930eef7d3
Khalid Alhokail <khalid@al-hokail.com>
2022-04-15 22:32:17 +0300

    Initial Commit

---
+++ Saudi Safe Sites/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Saudi Safe Sites
+//
+//  Created by Khalid AlHokail on 4/15/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
